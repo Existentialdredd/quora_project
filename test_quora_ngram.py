26a8c9235f58df90d91583f6bdb5606ac3c78064
Existentialdredd <eric.pnr@gmail.com>
2019-09-25 16:18:54 -0700

    added additive smoothing with tests and predict function

--- test_quora_ngram.py
+++ test_quora_ngram.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from collections import defaultdict
 from quora_ngram import NgramModel 
 
@@ -34,6 +35,51 @@ class TestQuoraNgram(unittest.TestCase):
         self.assertDictEqual(dict(self.vocab.trigram_counts[0]),trigram_counts_target[1])
         self.assertDictEqual(dict(self.vocab.trigram_counts[1]),trigram_counts_target[0])
         self.assertEqual(self.vocab.trigram_totals,[1,1])
+
+
+class TestQuoraNgram2(unittest.TestCase): 
+
+    def setUp(self):
+        comments = [['xxxxx',['where','are','you'],1],['yyyyy',['who','are','you'],0]]
+        self.vocab = NgramModel(comments)
+        
+
+    def tearDown(self):
+        self.vocab = None
+
+    def test_unigram_freq(self): 
+        self.vocab._additive_smoothing_(1,1)
+        unigram_freqs_target = [{'where':np.log((1+1)/(1*3+3)),
+                                  'are': np.log((1+1)/(1*3+3)),
+                                  'you':np.log((1+1)/(1*3+3)),
+                                  '<unk>':np.log((1+1)/(1*3+3))},
+                                 {'who':np.log((1+1)/(1*3+3)),
+                                  'are': np.log((1+1)/(1*3+3)),
+                                  'you':np.log((1+1)/(1*3+3)),
+                                  '<unk>':np.log((1+1)/(1*3+3))}]
+        self.assertDictEqual(self.vocab.gram_frequency[0],unigram_freqs_target[1])
+        self.assertDictEqual(self.vocab.gram_frequency[1],unigram_freqs_target[0])
+
+    def test_bigram_freq(self): 
+        self.vocab._additive_smoothing_(2,1)
+        bigram_freqs_target = [{'where_are':np.log((1+1)/(1*2+2)),
+                                  'are_you': np.log((1+1)/(1*2+2)),
+                                  '<unk>':np.log((1+1)/(1*2+2))},
+                                 {'who_are':np.log((1+1)/(1*2+2)),
+                                  'are_you': np.log((1+1)/(1*2+2)),
+                                  '<unk>':np.log((1+1)/(1*2+2))}]
+        self.assertDictEqual(self.vocab.gram_frequency[0],bigram_freqs_target[1])
+        self.assertDictEqual(self.vocab.gram_frequency[1],bigram_freqs_target[0])
+
+
+    def test_trigram_freq(self): 
+        self.vocab._additive_smoothing_(3,1)
+        trigram_freqs_target = [{'where_are_you':np.log((1+1)/(1*1+1)),
+                                  '<unk>':np.log((1+1)/(1*1+1))},
+                                 {'who_are_you':np.log((1+1)/(1*1+1)),
+                                  '<unk>':np.log((1+1)/(1*1+1))}]
+        self.assertDictEqual(self.vocab.gram_frequency[0],trigram_freqs_target[1])
+        self.assertDictEqual(self.vocab.gram_frequency[1],trigram_freqs_target[0])
 
 if __name__ == '__main__': 
     unittest.main()
