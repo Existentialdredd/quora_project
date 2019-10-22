135f1e8d390e6e862bcacbd4ed61cf676d068256
ericpnr <eric.pnr@gmail.com>
2019-10-22 12:40:35 -0700

    final changes

--- misc/cloud/instance_setup.sh
+++ misc/cloud/instance_setup.sh
@@ -9,8 +9,8 @@ pushd ${HOME}
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install bzip2
-sudo apt-get tmux
-sudo apt-get htop
+sudo apt-get install tmux
+sudo apt-get install htop
 
 ANACONDA_INSTALLER="Anaconda3-5.2.0-Linux-x86_64.sh"
 wget "https://repo.continuum.io/archive/$ANACONDA_INSTALLER"
@@ -29,8 +29,18 @@ cp -v $(dirname $0)/support/jupyter_notebook_config.py ${HOME}/.jupyter/jupyter_
 cd ${HOME}/quora/misc/
 conda env create -f enviro_debian.yml
 
+source activate quora_work
+
 cd ${HOME}/quora/eda
 gdown https://drive.google.com/uc?id=1DRNWMId4T-0qP6EGBmMqbPWErUpuSS-n
 cd ${HOME}/quora/embeddings
 gdown https://drive.google.com/uc?id=1yCmhJJq0uJhDgjlW4PzfXlXhbs4DpIIK
 cd ${HOME}
+
+git config --global user.email "eric.pnr@gmail.com"
+git config --global user.name "Existentialdredd"
+
+## Vim Setup Cmd Line
+# git clone https://github.com/Existentialdredd/vimrc.git ~/vimrc
+# cp ~/vimrc/.vimrc ~
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
