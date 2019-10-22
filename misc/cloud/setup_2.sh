bcfd5f711c40b50733bb5b86d8470af0aa423185
ericpnr <epenner@berkeley.edu>
2019-10-22 07:11:58 -0700

    modified setup, change enviro name

--- misc/cloud/setup_2.sh
+++ misc/cloud/setup_2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+#git clone https://github.com/Existantialdredd/vimrc.git ~/vimrc
+#cp ${HOME}/vimrc/.vimrc ~
+
+cd ${HOME}/quora/misc/
+conda env create -f enviro_debian.yml
+
 cd ${HOME}/quora/eda
 gdown https://drive.google.com/uc?id=1DRNWMId4T-0qP6EGBmMqbPWErUpuSS-n
 cd ${HOME}/quora/embeddings
 gdown https://drive.google.com/uc?id=1yCmhJJq0uJhDgjlW4PzfXlXhbs4DpIIK
 cd ${HOME}
-
-git clone https://github.com/Existantialdredd/vimrc.git ~/vimrc
-cp ${HOME}/vimrc/.vimrc ~
-
-cd ${HOME}/quora/misc/
-conda env create -f enviro_debian.yml
