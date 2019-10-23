a1755b2520bec829e4c58ae2fb4f5a1156675075
Existentialdredd <eric.pnr@gmail.com>
2019-10-23 20:36:22 +0000

    first run complete

--- neural_cnn/neural_cnn_runs.py
+++ neural_cnn/neural_cnn_runs.py
@@ -67,7 +67,7 @@ ncnn_classifier = ncnn.Neural_CNN({'n_tokens':len(embeddings)-1,
 ncnn_classifier.build_graph()
 
 
-train_dict = {'embeddings':embeddings,
+train_dict = {'embeddings':embeddings,'kernel_sizes_by_lyr':[[1,2,3,4]],
               'sequences_train':sequences_train,'labels_train': labels_train,
               'sequences_valid':sequences_valid,'labels_valid': labels_valid,'batch_size':100}
 
