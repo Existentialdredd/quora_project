b5bee0ba5f6d78e09543008f5bf32043d8ce2f44
ericpnr <epenner@berkeley.edu>
2019-10-26 14:51:21 -0700

    fixed instance connect and classifier training issue

--- misc/cloud/instance_connect.sh
+++ misc/cloud/instance_connect.sh
@@ -3,11 +3,11 @@
 read -p "Instance Number: " INST
 
 if [ $INST -eq 1 ]; then
-	gcloud compute ssh --ssh-flag="8896:127.0.0.1:8896" --ssh-flag="6006:127.0.0.1:6006" eric_pnr@instance-1 --zone=us-west1-a 
+	gcloud compute ssh --ssh-flag="-L 8896:127.0.0.1:8896" --ssh-flag="-L 6006:127.0.0.1:6006" eric_pnr@instance-1 --zone=us-west1-a 
 elif [ $INST -eq 2 ]; then
-	gcloud compute ssh --ssh-flag="8897:127.0.0.1:8896" --ssh-flag="6007:127.0.0.1:6006" eric_pnr@instance-2 --zone=us-west2-a 
+	gcloud compute ssh --ssh-flag="-L 8897:127.0.0.1:8896" --ssh-flag="-L 6007:127.0.0.1:6006" eric_pnr@instance-2 --zone=us-west2-a 
 elif [ $INST -eq 3 ]; then
-	gcloud compute ssh --ssh-flag="8898:127.0.0.1:8896" --ssh-flag="6008:127.0.0.1:6006" eric_pnr@instance-3 --zone=us-west2-a 
+	gcloud compute ssh --ssh-flag="-L 8898:127.0.0.1:8896" --ssh-flag="-L 6008:127.0.0.1:6006" eric_pnr@instance-3 --zone=us-west2-a 
 fi
 
 
