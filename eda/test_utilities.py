100ca7fd2a2c844ecbe7751650424f3544d42f49
ericpnr <epenner@berkeley.edu>
2020-02-05 13:28:46 -0800

    finished eda progress cleaned up vocab and utilities

--- eda/test_utilities.py
+++ eda/test_utilities.py
@@ -2,7 +2,7 @@ import unittest
 import utilities as ut
 
 class TestUtilities(unittest.TestCase):
-    
+
     def test_canonicalize_sentence(self):
         self.assertEquals(ut.canonicalize_sentence('word.other'),'word other')
         self.assertEquals(ut.canonicalize_sentence('word. other'),'word other')
@@ -14,8 +14,8 @@ class TestUtilities(unittest.TestCase):
         input = 'Who. are? you-today'
         target = ['who','are','you','today']
         self.assertEquals(ut.canon_token_sentence(input),target)
-        
 
-        
-if __name__ == '__main__': 
+
+
+if __name__ == '__main__':
    unittest.main()
