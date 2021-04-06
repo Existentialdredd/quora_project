dc05fb802a09346c2ea8bed69e588adb2a3f2854
Penner, Eric <eric.pnr@gmail.com>
2021-04-06 15:10:03 -0600

    added bert train_model method

--- hf_models/bert_model_runs.py
+++ hf_models/bert_model_runs.py
@@ -1,66 +1,57 @@
 import torch
-import random
-import numpy as np
-import csv
-import pandas as pd
 from torch.utils.data import DataLoader
 from torch.nn import functional
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW
-from data.pytorch_dataset import QuoraBertDataSet, bert_tensor_colate
-from pprint import pformat
+from transformers import BertForSequenceClassification, AdamW
+from hf_utilities import QuoraBertDataSet, bert_tensor_colate
+from data.schemas import ModelState
 
-train_data = QuoraBertDataSet('train_neg_50k.csv', 'train_pos_50k.csv', data_slice=[0, 100])
-valid_data = QuoraBertDataSet('train_neg_50k.csv', 'train_pos_50k.csv', data_slice=[101, 201])
-train_dataloader = DataLoader(train_data, batch_size=50, collate_fn=bert_tensor_colate, shuffle=True)
-valid_dataloader = DataLoader(valid_data, batch_size=len(valid_data), collate_fn=bert_tensor_colate)
 
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', return_dict=True)
-for param in model.base_model.parameters():
-    param.requires_grad = False
+def train_model(train_config_json, valid_config_json, model_name='bert-base-uncased', n_epochs=1):
+    """
+    PURPOSE:
+    """
+    train_data = QuoraBertDataSet(config_json=train_config_json)
+    valid_data = QuoraBertDataSet(config_json=valid_config_json)
 
-optimizer = AdamW(model.parameters(), lr=1e-5)
+    train_dataloader = DataLoader(train_data, batch_size=50, collate_fn=bert_tensor_colate, shuffle=True)
+    valid_dataloader = DataLoader(valid_data, batch_size=len(valid_data), collate_fn=bert_tensor_colate)
 
-N_epochs = 2
+    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', return_dict=True)
+    for param in model.base_model.parameters():
+        param.requires_grad = False
 
-train_state = {
-     'epoch_index':  0,
-     'train_loss':  [],
-     'train_acc':  [],
-     'val_loss': [],
-     'val_acc':  [],
-     'test_loss': -1,
-     'test_acc': -1
-     }
+    optimizer = AdamW(model.parameters(), lr=1e-5)
+    train_state = ModelState()
 
-for epoch in range(N_epochs):
-    model.train()
-    for batch in train_dataloader:
-        optimizer.zero_grad()
-        model_output = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask'])
-        loss = functional.binary_cross_entropy_with_logits(model_output.logits, batch['label'])
-        loss.backward()
-        optimizer.step()
-        train_accuracy = torch.eq(torch.argmax(model_output.logits, dim=1),
-                                  torch.argmax(batch['label'], dim=1)).float().mean()
-        train_loss = loss.data.item()
-        train_state['train_loss'].append(train_loss)
-        train_state['train_acc'].append(train_accuracy.item())
+    for epoch in range(n_epochs):
+        model.train()
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            # import pdb; pdb.set_trace()
+            model_output = model(input_ids=batch['token_ids'], attention_mask=batch['attention_mask'])
+            loss = functional.binary_cross_entropy_with_logits(model_output.logits, batch['label'])
+            loss.backward()
+            optimizer.step()
+            train_accuracy = torch.eq(torch.argmax(model_output.logits, dim=1),
+                                      torch.argmax(batch['label'], dim=1)).float().mean()
+            train_loss = loss.data.item()
+            train_state.train_loss.append(train_loss)
+            train_state.train_acc.append(train_accuracy.item())
 
-    model.eval()
-    for valid_batch in valid_dataloader:
-        model_output = model(input_ids=valid_batch['input_ids'], attention_mask=valid_batch['attention_mask'])
-        loss = functional.binary_cross_entropy_with_logits(model_output.logits, valid_batch['label'])
-        valid_accuracy = torch.eq(torch.argmax(model_output.logits, dim=1),
-                                  torch.argmax(valid_batch['label'], dim=1)).float().mean()
-        valid_loss = loss.data.item()
+        model.eval()
+        for valid_batch in valid_dataloader:
+            model_output = model(input_ids=valid_batch['token_ids'], attention_mask=valid_batch['attention_mask'])
+            loss = functional.binary_cross_entropy_with_logits(model_output.logits, valid_batch['label'])
+            valid_accuracy = torch.eq(torch.argmax(model_output.logits, dim=1),
+                                      torch.argmax(valid_batch['label'], dim=1)).float().mean()
+            valid_loss = loss.data.item()
 
-        train_state['val_loss'].append(valid_loss)
-        train_state['val_acc'].append(valid_accuracy.item())
+            train_state.val_loss.append(valid_loss)
+            train_state.val_acc.append(valid_accuracy.item())
 
-    print(f"Epoch: {epoch}")
-    print(f"Train Loss: {train_state['train_loss'][-1]}")
-    print(f"Train Acc: {train_state['train_acc'][-1]}")
-    print(f"Valid Loss: {train_state['val_loss'][-1]}")
-    print(f"Valid Acc: {train_state['val_acc'][-1]}")
+        print(f"Epoch: {epoch}")
+        print(f"Train Loss: {train_state.train_loss[-1]}")
+        print(f"Train Acc: {train_state.train_acc[-1]}")
+        print(f"Valid Loss: {train_state.val_loss[-1]}")
+        print(f"Valid Acc: {train_state.val_acc[-1]}")
 
-print(f" {pformat(train_state)}")
